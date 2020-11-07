5e5301849ad8792b16b603cb7791f63b14ea4d45
Marat <imd_90@mail.ru>
2020-11-07 23:34:33 +0300

    updated NPSService

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int[] points = new int[]{10, 2, 8, 9, 9, 6, 7, 9, 10, 8};
+        int[] points = new int[]{10, 2, 8, 9, 9, 6, 7, 9, 10, 8, 1, 10, 6, 9, 9};
+        int neutralsLow = 7;
+        int neutralsUp = 8;
 
         int length = points.length;
 
         int promoters =0;
             for (int point : points) {
-                if (point > 8) {
+                if (point > neutralsUp) {
                     promoters = promoters + 1;
             }
         }
 
         int detractores = 0;
         for (int point : points) {
-            if (point < 7) {
+            if (point < neutralsLow) {
                 detractores = detractores +1;
             }
         }
